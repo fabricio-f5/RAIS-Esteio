dcf1d092fc20a7ceb054fd4c865f00220f16bc69
TonioloF <74147118+TonioloF@users.noreply.github.com>
2021-07-14 11:11:41 -0300

    Novas Atualizações

--- ScriptsSQL/Arquitetura/script_inser_config_parameters.sql
+++ ScriptsSQL/Arquitetura/script_inser_config_parameters.sql
@@ -8,7 +8,7 @@ INSERT INTO `db_webapp`.`configparameter`
 (`parameter`,`description`,`parameterValue`) VALUES ('scanTimeMonitoringKey','Tempo de Varredura - Monitoramento dos Claviculários.',3000);
 
 INSERT INTO `db_webapp`.`configparameter`
-(`parameter`,`description`,`parameterValue`) VALUES ('SupervisorEmail','E-mail destino para mensagens de aviso caso o usuário pegue uma chave que não seja do seu perfil de acesso.','adenirtoniolofilho@gmail.com');
+(`parameter`,`description`,`parameterValue`) VALUES ('SupervisorEmailClaviculario','E-mail destino para mensagens de aviso caso o usuário pegue uma chave que não seja do seu perfil de acesso.','adenirtoniolofilho@gmail.com');
 
 INSERT INTO `db_webapp`.`configparameter`
 (`parameter`,`description`,`parameterValue`) VALUES ('PurgeReport','Período de retenção dos dados do relatório em dias.',100);
