cdbb5777ad94bad8b3f814e6ccc04611aeb2e9bc
TonioloF <74147118+TonioloF@users.noreply.github.com>
2021-07-19 14:40:02 -0300

    Novas Atualizações

--- ScriptsSQL/Arquitetura/script_create_ownerUser.sql
+++ ScriptsSQL/Arquitetura/script_create_ownerUser.sql
@@ -1,7 +1,7 @@
 USE db_webapp;
-delete from user where email = 'adenirtoniolofilho@gmail.com';
+delete from user where email = 'rais.terminal@f5automation.com.br';
 Select  @profile_id := id from db_webapp.profile Where profileNumber = 0;
 INSERT INTO db_webapp.user  (`profile_id`,`name`,`phone`,`email`,`cpf`,`password`,`status`,`restartPassword`,`lastLogin`,`lastLogout`,`activated`,`logonUser`,`rfid`) VALUES
-( @profile_id,'Adenir Toniolo Filho','11984279274','adenirtoniolofilho@gmail.com','21353108291','91797bf249fe60f3a833676b3a0500b9',0,0,null,null,1,'Toniolo',null);
+( @profile_id,'F5 Automação',null,'rais.terminal@f5automation.com.br',null,'91797bf249fe60f3a833676b3a0500b9',0,0,null,null,1,'f5automation',null);
 select * from user;
 
